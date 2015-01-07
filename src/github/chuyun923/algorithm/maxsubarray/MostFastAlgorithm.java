166036c0736662c1201da401b246b05bedee4b3f
彭亮 <pengliang02@meituan.com>
2015-01-07 11:28:21 +0800

    最快的最大子数组算法

---
+++ src/github/chuyun923/algorithm/maxsubarray/MostFastAlgorithm.java
@@ -0,0 +1,44 @@
+package github.chuyun923.algorithm.maxsubarray;
+
+/**
+ * Created by pengliang on 15-1-7.
+ *
+ * 线性时间处理最大子串的问题，思路:
+ *
+ * 从第一个元素开始，依次加上后面一个元素，直到总和少于0，在重新从下一个元素开始循环
+ * 原理：
+ *
+ *       * * * * * * * * *
+ *         ——————
+ *
+ * 如果带下划线的子串是最大子串，那么子串前面和后面的元素之和应该都是负数(注意这里没有考虑全负数的情况)
+ *
+ */
+
+public class MostFastAlgorithm extends MaxSubArrayAl {
+    @Override
+    public SubArray findMaxMumSubArray(int[] array) {
+        //最大的子串的和
+        SubArray subArray = new SubArray();
+        subArray.sum = Integer.MIN_VALUE;
+        int sum = array[0];
+        int left = 0;
+        subArray.left = subArray.right = 0;
+
+        for(int i = 1;i<array.length;i++) {
+            if(sum<0) {
+                sum = array[i];
+                left = i;
+            }else {
+                sum += array[i];
+            }
+            
+            if(sum>subArray.sum) {
+                subArray.left = left;
+                subArray.right = i;
+                subArray.sum = sum;
+            }
+        }
+        return subArray;
+    }
+}
--- src/github/chuyun923/algorithm/maxsubarray/Test.java
+++ src/github/chuyun923/algorithm/maxsubarray/Test.java
@@ -5,15 +5,17 @@ package github.chuyun923.algorithm.maxsubarray;
  */
 class Test {
     public static void main(String args[]) {
-        int array[] = {-1 ,-4 ,2 ,-1 ,3,-1};
+        int array[] = {-1 ,4 ,-2 ,-1 ,-3,-1};
         MaxSubArrayAl maxSubArrayAl = new BruteForceAlgorithm();
         MaxSubArrayAl maxSubArrayAl1 = new DivideConquerAlgorithm();
         MaxSubArrayAl maxSubArrayAl2 = new MostBruteForceAlgorithm();
+        MaxSubArrayAl maxSubArrayAl3 = new MostFastAlgorithm();
         SubArray subArray = maxSubArrayAl.findMaxMumSubArray(array);
         SubArray subArray1 = maxSubArrayAl1.findMaxMumSubArray(array);
         SubArray subArray2 = maxSubArrayAl2.findMaxMumSubArray(array);
+        SubArray subArray3 = maxSubArrayAl3.findMaxMumSubArray(array);
 //        subArray.printInfo();
 //        subArray1.printInfo();
-        subArray2.printInfo();
+        subArray3.printInfo();
     }
 }
